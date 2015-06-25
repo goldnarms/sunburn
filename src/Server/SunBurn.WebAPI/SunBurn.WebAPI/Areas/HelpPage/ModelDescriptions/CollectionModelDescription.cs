11b9bee3fb40541a89d3f43e3bfb4c26cc31ea60
Arnstein Johansen <arnstej@gmail.com>
2015-06-25 20:36:46 +0200

    Added project for web api

---
+++ src/Server/SunBurn.WebAPI/SunBurn.WebAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace SunBurn.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ src/Server/SunBurn.WebAPI/SunBurn.WebAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SunBurn.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
